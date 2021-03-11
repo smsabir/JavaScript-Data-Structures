e4cece55b8583960ea3d8c9b8d0bc3e9842ebf1a
S M Abdullah Al Sabir <sabir.smaa2@gmail.com>
2021-03-11 09:10:35 +0100

    complex multi-dimensional arrays

---
+++ create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,15 @@
+let myNestedArray = [
+  // Only change code below this line
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  ['mutate', 1327.98, 'splice', 'slice', 'push'],
+  [ 
+    ['loop', 'shift', 6, 7, 1000, 'method', 'deep'],
+    [ 
+      ['concat', false, true, 'spread', 'array', 'deeper'],
+      [
+        ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', 'deepest'],
+      ]
+    ]
+  ]
+  // Only change code above this line
+];
