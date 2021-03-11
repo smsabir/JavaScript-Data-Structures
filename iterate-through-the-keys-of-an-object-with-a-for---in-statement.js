0a5f68aeb83fe6aa8668c7395ab3f766c5577803
S M Abdullah Al Sabir <sabir.smaa2@gmail.com>
2021-03-11 11:54:32 +0100

    Iterate Through the Keys of an Object with a for

---
+++ iterate-through-the-keys-of-an-object-with-a-for---in-statement.js
@@ -0,0 +1,14 @@
+function countOnline(usersObj) {
+
+  // Only change code below this line
+  let count = 0;
+  for(let user in usersObj){
+    if(usersObj[user].online === true){
+      count = count + 1;
+    }
+  }
+  return count;
+  // Only change code above this line
+}
+
+
